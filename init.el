40e1bcf6c8a908e891e31f9fc32cb8d752504f1e
Max Miorim <max@miorim.com>
2018-08-17 08:58:52 -0300

    Enable eldoc in cider-mode

--- init.el
+++ init.el
@@ -69,7 +69,8 @@
             (add-hook 'clojure-mode-hook 'cider-mode)
             (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
             (add-hook 'cider-repl-mode-hook #'eldoc-mode)
-            (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)))
+            (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+            (add-hook 'cider-mode-hook #'eldoc-mode)))
 
 (use-package ensime)
 
